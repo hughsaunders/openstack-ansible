d9a9283f994a739efd8cc7ea4cae11af6366cd45
Jesse Pretorius <jesse.pretorius@rackspace.co.uk>
2015-02-04 15:51:06 +0000

    Implement local scripts for all gate tests
    This patch implements the following:
     - scripts-library.sh which includes commonly used functions, variables
       and other preparation commands for all other scripts
     - bootstrap-ansible.sh which only deploys a selected version of ansible
       and ensures that any other requirements are prepared on the
       deployment host
     - bootstrap-aio.sh which runs all host preparation actions for an
       all-in-one build
     - gate-check-lint.sh which runs a lint and syntax check
     - gate-check-commit.sh which runs all actions required for a gate
       commit check, utilising the other scripts where required
     - run-smoke-test.sh which runs tempest from inside the utility container
     - run-playbooks.sh which runs the playbooks
     - the existing conf.d/swift.yml is renamed to be an example
       configuration - the example configurations can be used as
       documentation
     - etc/network/interfaces.d/aio_interfaces.cfg,
       etc/rpc_deploy/conf.d/swift.yml and
       etc/rpc_deploy/rpc_user_config.yml are now configurations used for
       the AIO deployment
     - a workaround for https://bugs.launchpad.net/bugs/1244589 to ensure
       that DHCP checksums are implemented by the host which is required for
       the smoke tests to work
     - the removal of the rpc heat templates as they're unusable in their
       current state
     - setting MAX_RETRIES to 0, ensuring that any failures cause an
       immediate commit check failure in the gate - this prevents the
       masking of failures by retry attempts
    
    DocImpact
    Co-Authored-By: Kevin Carter <kevin.carter@rackspace.com>
    Closes-Bug: #1415883
    Closes-Bug: #1417999
    Closes-Bug: #1419807
    Change-Id: I95242d48ad0fb055f16510803c8aa14dc183ac17

--- scripts/run-aio-build.sh
+++ scripts/run-aio-build.sh
@@ -12,11 +12,22 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-set -e -u -v -x
+
+## Shell Opts ----------------------------------------------------------------
+
+set -e -u -v +x
+
+## Variables -----------------------------------------------------------------
 
 REPO_URL=${REPO_URL:-"https://github.com/stackforge/os-ansible-deployment.git"}
 REPO_BRANCH=${REPO_BRANCH:-"master"}
 WORKING_FOLDER=${WORKING_FOLDER:-"/opt/stackforge/os-ansible-deployment"}
+ANSIBLE_PARAMETERS=${ANSIBLE_ANSIBLE_PARAMETERS:-"--forks 10"}
+
+## Main ----------------------------------------------------------------------
+
+# set verbosity
+set -x
 
 # install git so that we can fetch the repo
 apt-get update && apt-get install -y git
@@ -24,19 +35,31 @@ apt-get update && apt-get install -y git
 # fetch the repo
 git clone -b ${REPO_BRANCH} ${REPO_URL} ${WORKING_FOLDER}/
 
-# put an motd in place to help the user know how to restart galera after reboot
+# run the same aio build script that is used in the OpenStack CI pipeline
+cd ${WORKING_FOLDER}
+
+source $(dirname ${0})/gate-check-commit.sh
+
+# put a motd in place to help the user know what stuff is accessible once the build is complete
 cat > /etc/update-motd.d/20-openstack<< EOF
 #!/usr/bin/env bash
 echo ""
-echo "############ ansible-os-deployment all-in-one build #############"
+echo "############ os-ansible-deployment all-in-one build #############"
+echo ""
+echo " OpenStack Services are now listening on $(ip -o -4 addr show dev eth0 | awk -F '[ /]+' '/global/ {print $4}')"
+echo ""
+EOF
+chmod +x /etc/update-motd.d/20-openstack
+
+# put an motd in place to help the user know how to restart galera after reboot
+cat > /etc/update-motd.d/21-galera<< EOF
+#!/usr/bin/env bash
+echo ""
 echo "If this server has been rebooted, you will need to re-bootstrap"
 echo "Galera to get the cluster operational. To do this execute:"
 echo ""
 echo "cd /opt/ansible-lxc-rpc/rpc_deployment"
 echo "ansible-playbook -e @/etc/rpc_deploy/user_variables.yml playbooks/infrastructure/galera-startup.yml"
+echo ""
 EOF
-chmod +x /etc/update-motd.d/20-openstack
-
-# run the same aio build script that is used in the OpenStack CI pipeline
-cd ${WORKING_FOLDER}
-./scripts/os-ansible-aio-check.sh
+chmod +x /etc/update-motd.d/21-galera
